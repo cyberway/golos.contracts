3ca58d5c2be10f7108932135d567a74c38a99e37
Alexander Dedkov <sanya.dedckov@yandex.ru>
2018-10-22 07:14:42 +0300

    testing
    issue #113

--- tests/golos.vesting_tests.cpp
+++ tests/golos.vesting_tests.cpp
@@ -1,8 +1,15 @@
-#include "golos_tester.hpp"
-#include "contracts.hpp"
+#include <boost/test/unit_test.hpp>
+#include <eosio/testing/tester.hpp>
+#include <eosio/chain/abi_serializer.hpp>
 #include <Runtime/Runtime.h>
 
 #include <fc/variant_object.hpp>
+
+#include "eosio.token/eosio.token.abi.hpp"
+#include "eosio.token/eosio.token.wast.hpp"
+
+#include "golos.vesting/golos.vesting.abi.hpp"
+#include "golos.vesting/golos.vesting.wast.hpp"
 
 using namespace eosio::testing;
 using namespace eosio;
@@ -13,21 +20,33 @@ using namespace std;
 
 using mvo = fc::mutable_variant_object;
 
-class golos_vesting_tester : public golos_tester {
+class golos_vesting_tester : public tester {
 public:
 
-    golos_vesting_tester(): golos_tester() {
-        produce_blocks(2);
+    golos_vesting_tester() {
+        produce_blocks( 2 );
 
-        create_accounts( { N(sania), N(pasha), N(golos.vest), N(golos.ctrl), N(eosio.token), N(golos.emiss), N(golos.issuer) } );
-        produce_blocks(2);
+        create_accounts( { N(sania), N(tanya), N(pasha), N(tanya), N(golos.vest), N(eosio.token), N(golos.emiss), N(golos.issuer), N(golos.ctrl) } );
+        produce_blocks( 2 );
 
-        install_contract(N(eosio.token), contracts::token_wasm(), contracts::token_abi(), abi_ser_t);
-        install_contract(N(golos.vest), contracts::vesting_wasm(), contracts::vesting_abi(), abi_ser_v);
-        abi_serializer t;
-        install_contract(N(golos.ctrl), contracts::ctrl_wasm(), contracts::ctrl_abi(), t);
+        set_code(N(eosio.token), eosio_token_wast);
+        set_abi (N(eosio.token), eosio_token_abi);
+
+        set_code(N(golos.vest), golos_vesting_wast);
+        set_abi (N(golos.vest), golos_vesting_abi);
+
+        produce_blocks();
+
+        const auto& account_token = control->db().get<account_object,by_name>( N(eosio.token) );
+        abi_def abi_t;
+        BOOST_REQUIRE_EQUAL(abi_serializer::to_abi(account_token.abi, abi_t), true);
+        abi_ser_t.set_abi(abi_t, abi_serializer_max_time);
+
+        const auto& account_vesting = control->db().get<account_object,by_name>( N(golos.vest) );
+        abi_def abi_v;
+        BOOST_REQUIRE_EQUAL(abi_serializer::to_abi(account_vesting.abi, abi_v), true);
+        abi_ser_v.set_abi(abi_v, abi_serializer_max_time);
     }
-
     action_result push_action( const account_name& signer, const action_name &name, const variant_object &data ) {
         string action_type_name = abi_ser_t.get_action_type(name);
 
@@ -181,7 +200,6 @@ BOOST_FIXTURE_TEST_CASE( test_create_vesting, golos_vesting_tester ) try {
     BOOST_REQUIRE_EQUAL( success(), create_vesting_token(N(golos.issuer), symbol(4,"GOLOS")));
 } FC_LOG_AND_RETHROW()
 
-
 BOOST_FIXTURE_TEST_CASE( test_issue_tokens_accounts, golos_vesting_tester ) try {
     BOOST_REQUIRE_EQUAL( success(), create(N(golos.issuer), asset::from_string("100000.0000 GOLOS")) );
 
@@ -194,7 +212,7 @@ BOOST_FIXTURE_TEST_CASE( test_issue_tokens_accounts, golos_vesting_tester ) try 
                              ("supply", "1000.0000 GOLOS")
                              ("max_supply", "100000.0000 GOLOS")
                              ("issuer", "golos.issuer")
-                             ); 
+                             );
 
     auto sania_balance = get_account(N(sania), "4,GOLOS");
     REQUIRE_MATCHING_OBJECT( sania_balance, mvo()
