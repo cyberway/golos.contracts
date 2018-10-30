3bff73e7b3ea08d4d04accce02837b414950e478
DedbaradeD <gavrilkoo@gmail.com>
2018-10-30 12:39:37 +0100

    remarks correction2 #139 #140

--- scripts/accounts_and_keys.py
+++ scripts/accounts_and_keys.py
@@ -1,12 +1,136 @@
 import dbs
 import bson
 
+golos_authority_keys = dbs.golos_db['account_authority_object']
+cyberway_permusage = dbs.cyberway_db['permusage']
+cyberway_permission = dbs.cyberway_db['permission']
+
+def save_authority(doc):
+    if (cyberway_permission.find({"owner":doc["account"], "name":"owner"}).count() or 
+            cyberway_permission.find({"owner":doc["account"], "name":"active"}).count() or 
+            cyberway_permission.find({"owner":doc["account"], "name":"posting"}).count()):
+        return
+
+    default_id_count_permusage = cyberway_permusage.find().sort('id', -1).limit(1)[0]['id']
+    id_count_permusage = default_id_count_permusage + 1
+    id_count_permission = cyberway_permission.find().sort('id', -1).limit(1)[0]['id'] + 1
+    id_count_parent = cyberway_permission.find().sort('parent', -1).limit(1)[0]['parent'] + 1
+
+    owner_permusage = {
+	"id" : bson.Int64(id_count_permusage),
+	"last_used" : "",
+	"_SCOPE_" : "_CYBERWAY_",
+	"_PAYER_" : "",
+	"_SIZE_" : bson.Int64(16)
+    }
+    dbs.cyberway_db['permusage'].save(owner_permusage)
+    id_count_permusage += 1
+
+    owner_permission = {
+	"id" : bson.Int64(id_count_permission),
+	"usage_id" : owner_permusage["id"],
+	"parent" : bson.Int64(id_count_parent),
+	"owner" : doc["account"],
+	"name" : "owner",
+	"last_updated" : doc["last_owner_update"],
+	"auth" : {
+	    "threshold" : bson.Int64(1),
+	    "keys" : [
+		{
+		    "key" : doc["owner"],
+		    "weight" : bson.Int64(1)
+		}
+	    ],
+	    "accounts" : [ ],
+	    "waits" : [ ]
+	},
+	"_SCOPE_" : "_CYBERWAY_",
+	"_PAYER_" : "",
+	"_SIZE_" : bson.Int64(91)
+    }
+    dbs.cyberway_db['permission'].save(owner_permission)
+    id_count_permission += 1
+
+    active_permusage = {
+	"id" : bson.Int64(id_count_permusage),
+	"last_used" : "",
+	"_SCOPE_" : "_CYBERWAY_",
+	"_PAYER_" : "",
+	"_SIZE_" : bson.Int64(16)
+    }
+    dbs.cyberway_db['permusage'].save(active_permusage)
+    id_count_permusage += 1
+
+    active_permission = {
+	"id" : bson.Int64(id_count_permission),
+	"usage_id" : active_permusage["id"],
+	"parent" : bson.Int64(id_count_parent),
+	"owner" : doc["account"],
+	"name" : "active",
+	"last_updated" : "",
+	"auth" : {
+	    "threshold" : bson.Int64(1),
+	    "keys" : [
+		{
+		    "key" : doc["active"],
+		    "weight" : bson.Int64(1)
+		}
+	    ],
+	    "accounts" : [ ],
+	    "waits" : [ ]
+	},
+	"_SCOPE_" : "_CYBERWAY_",
+	"_PAYER_" : "",
+	"_SIZE_" : bson.Int64(91)
+    }
+    dbs.cyberway_db['permission'].save(active_permission)
+    id_count_permission += 1
+
+    posting_permusage = {
+	"id" : bson.Int64(id_count_permusage),
+	"last_used" : "",
+	"_SCOPE_" : "_CYBERWAY_",
+	"_PAYER_" : "",
+	"_SIZE_" : bson.Int64(16)
+    }
+    dbs.cyberway_db['permusage'].save(posting_permusage)
+    id_count_permusage += 1
+        
+    posting_permission = {
+	"id" : bson.Int64(id_count_permission),
+	"usage_id" : posting_permusage["id"],
+	"parent" : bson.Int64(id_count_parent),
+	"owner" : doc["account"],
+	"name" : "posting",
+	"last_updated" : "",
+	"auth" : {
+	    "threshold" : bson.Int64(1),
+	    "keys" : [
+		{
+		    "key" : doc["posting"],
+		    "weight" : bson.Int64(1)
+		}
+	    ],
+	    "accounts" : [ ],
+	    "waits" : [ ]
+	},
+	"_SCOPE_" : "_CYBERWAY_",
+	"_PAYER_" : "",
+	"_SIZE_" : bson.Int64(91)
+    }
+    dbs.cyberway_db['permission'].save(posting_permission)
+    id_count_permission += 1
+
+    id_count_parent += 1
+
 def convert_accounts_and_keys(argv = -1):
     golos_accounts = dbs.golos_db['account_object']
     cyberway_accounts = dbs.cyberway_db['account']
     
     default_id_count = cyberway_accounts.find().sort('id', -1).limit(1)[0]['id']
     id_count = default_id_count + 1
+
+    accounts_list = []
 
     for doc in golos_accounts.find({"name":{"$regex":"^[a-z]*([\.]|[a-z]|[1-5])*[a-o\.]$"}, 
         "$expr": { "$lte": [ { "$strLenCP": "$name" }, 12 ]}}):
@@ -32,6 +156,8 @@ def convert_accounts_and_keys(argv = -1):
             "_SIZE_" : bson.Int64(65)
         }
         dbs.cyberway_db['account'].save(account)
+        if int(argv):
+            accounts_list.append(doc["name"])
 
         accountseq = {
             "id" : bson.Int64(id_count),
@@ -81,112 +207,14 @@ def convert_accounts_and_keys(argv = -1):
 
         id_count += 1
 
-
-    golos_authority_keys = dbs.golos_db['account_authority_object']
-    cyberway_permusage = dbs.cyberway_db['permusage']
-    cyberway_permission = dbs.cyberway_db['permission']
-
-    default_id_count_permusage = cyberway_permusage.find().sort('id', -1).limit(1)[0]['id']
-    id_count_permusage = default_id_count_permusage + 1
-    id_count_permission = cyberway_permission.find().sort('id', -1).limit(1)[0]['id'] + 1
-    id_count_parent =cyberway_permission.find().sort('parent', -1).limit(1)[0]['parent'] + 1
-
-    for doc in golos_authority_keys.find({"account":{"$regex":"^[a-z]*([\.]|[a-z]|[1-5])*[a-o\.]$"}, 
-        "$expr": { "$lte": [ { "$strLenCP": "$account" }, 12 ]}}):
-        if (cyberway_permission.find({"owner":doc["account"], "name":"owner"}).count() or 
-                cyberway_permission.find({"owner":doc["account"], "name":"active"}).count() or 
-                cyberway_permission.find({"owner":doc["account"], "name":"posting"}).count()):
-            continue
-
-        if id_count_permusage - 1 - default_id_count_permusage == int(argv):
-            break
-
-        permusage = {
-	    "id" : bson.Int64(id_count_permusage),
-	    "last_used" : "",
-	    "_SCOPE_" : "_CYBERWAY_",
-	    "_PAYER_" : "",
-	    "_SIZE_" : bson.Int64(16)
-        }
-        dbs.cyberway_db['permusage'].save(permusage)
-
-        owner_permission = {
-	    "id" : bson.Int64(id_count_permission),
-	    "usage_id" : permusage["id"],
-	    "parent" : bson.Int64(id_count_parent),
-	    "owner" : doc["account"],
-	    "name" : "owner",
-	    "last_updated" : doc["last_owner_update"],
-	    "auth" : {
-		"threshold" : bson.Int64(1),
-		"keys" : [
-		    {
-			"key" : doc["owner"],
-			"weight" : bson.Int64(1)
-		    }
-		],
-		"accounts" : [ ],
-		"waits" : [ ]
-	    },
-	    "_SCOPE_" : "_CYBERWAY_",
-	    "_PAYER_" : "",
-	    "_SIZE_" : bson.Int64(91)
-        }
-        dbs.cyberway_db['permission'].save(owner_permission)
-        id_count_permission += 1
-
-        active_permission = {
-	    "id" : bson.Int64(id_count_permission),
-	    "usage_id" : permusage["id"],
-	    "parent" : bson.Int64(id_count_parent),
-	    "owner" : doc["account"],
-	    "name" : "active",
-	    "last_updated" : "",
-	    "auth" : {
-		"threshold" : bson.Int64(1),
-		"keys" : [
-		    {
-			"key" : doc["active"],
-			"weight" : bson.Int64(1)
-		    }
-		],
-		"accounts" : [ ],
-		"waits" : [ ]
-	    },
-	    "_SCOPE_" : "_CYBERWAY_",
-	    "_PAYER_" : "",
-	    "_SIZE_" : bson.Int64(91)
-        }
-        dbs.cyberway_db['permission'].save(active_permission)
-        id_count_permission += 1
-        
-        posting_permission = {
-	    "id" : bson.Int64(id_count_permission),
-	    "usage_id" : permusage["id"],
-	    "parent" : bson.Int64(id_count_parent),
-	    "owner" : doc["account"],
-	    "name" : "posting",
-	    "last_updated" : "",
-	    "auth" : {
-		"threshold" : bson.Int64(1),
-		"keys" : [
-		    {
-			"key" : doc["posting"],
-			"weight" : bson.Int64(1)
-		    }
-		],
-		"accounts" : [ ],
-		"waits" : [ ]
-	    },
-	    "_SCOPE_" : "_CYBERWAY_",
-	    "_PAYER_" : "",
-	    "_SIZE_" : bson.Int64(91)
-        }
-        dbs.cyberway_db['permission'].save(posting_permission)
-
-        id_count_permusage += 1
-        id_count_permission += 1
-        id_count_parent += 1
+    if len(accounts_list):
+        for account in accounts_list:
+            for doc in golos_authority_keys.find({"account":account}):
+                save_authority(doc)
+    else:
+        for doc in golos_authority_keys.find({"account":{"$regex":"^[a-z]*([\.]|[a-z]|[1-5])*[a-o\.]$"}, 
+            "$expr": { "$lte": [ { "$strLenCP": "$account" }, 12 ]}}):
+            save_authority(doc)
 
     return True
 
