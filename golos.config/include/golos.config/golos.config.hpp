4777c6ad7bd926b998c0d617384770bd56cd5675
zxcat <550974+zxcat@users.noreply.github.com>
2018-11-30 15:07:29 +0300

    Update config smart-contract to support CDT 1.4 #310

--- golos.config/include/golos.config/golos.config.hpp
+++ golos.config/include/golos.config/golos.config.hpp
@@ -12,20 +12,20 @@ using namespace eosio;
 // NOTE: not finished yet, delayed until we require median, etc…
 class configer: public contract {
 public:
-    configer(account_name self);
+    using contract::contract;
 
     // own parameters
     [[eosio::action]] void validateprms(std::vector<cfg_param>);
     [[eosio::action]] void setparams(std::vector<cfg_param>);
 
     // base for recalculatable parameters
-    [[eosio::action]] void updateparams(account_name, std::vector<cfg_param>);
-    [[eosio::action]] void updateparamse(account_name, std::vector<emit_param>);    // TODO: maybe combine into 1 action accepting all params
-    [[eosio::action]] void notifytop(std::vector<account_name>);
+    [[eosio::action]] void updateparams(name, std::vector<cfg_param>);
+    [[eosio::action]] void updateparamse(name, std::vector<emit_param>);    // TODO: maybe combine into 1 action accepting all params
+    [[eosio::action]] void notifytop(std::vector<name>);
 
 private:
-    std::vector<account_name> get_top_witnesses();
-    bool is_top_witness(account_name account);
+    std::vector<name> get_top_witnesses();
+    bool is_top_witness(name account);
     void recalculate_state(std::vector<emit_param>);
 };
 
