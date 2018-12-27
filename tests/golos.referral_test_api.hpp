7277dfaccf858bbf527f4ab75525a03752a7644f
Alexander Dedkov <sanya.dedckov@yandex.ru>
2018-12-27 14:09:18 +0300

    348 settings for referral program (#384)

--- tests/golos.referral_test_api.hpp
+++ tests/golos.referral_test_api.hpp
@@ -4,9 +4,28 @@
 
 namespace eosio { namespace testing {
 
+class extended_tester : public golos_tester {
+    using golos_tester::golos_tester;
+    fc::microseconds _cur_time;
+    void update_cur_time() { _cur_time = control->head_block_time().time_since_epoch();};
+
+protected:
+    const fc::microseconds& cur_time()const { return _cur_time; };
+
+public:
+    void step(uint32_t n = 1) {
+        produce_blocks(n);
+        update_cur_time();
+    }
+
+    void run(const fc::microseconds& t) {
+        _produce_block(t);  // it produces only 1 block. this can cause expired transactions. use step() to push current txs into blockchain
+        update_cur_time();
+    }
+};
 
 struct golos_referral_api: base_contract_api {
-    golos_referral_api(golos_tester* tester, name code)
+    golos_referral_api(extended_tester* tester, name code)
         :   base_contract_api(tester, code) {}
 
     //// referral actions
@@ -20,6 +39,36 @@ struct golos_referral_api: base_contract_api {
          );
      }
 
+     action_result close_old_referrals() {
+         return push(N(closeoldref), _code, args()
+             ("hash", rand())
+         );
+     }
+
+     action_result set_params(name creator, std::string json_params) {
+         return push(N(setparams), creator, args()
+             ("params", json_str_to_obj(json_params)));
+     }
+
+     variant get_params() const {
+         return base_contract_api::get_struct(_code, N(refparams), N(refparams), "referral_state");
+     }
+
+     string breakout_parametrs(asset min_breakout, asset max_breakout) {
+         return string("['breakout_parametrs', {'min_breakout':'") + min_breakout.to_string() + "','max_breakout':'" + max_breakout.to_string() + "'}]";
+     }
+
+     string expire_parametrs(uint64_t max_expire) {
+         return string("['expire_parametrs', {'max_expire':'") + std::to_string(max_expire) + "'}]";
+     }
+
+     string percent_parametrs(uint32_t max_perсent) {
+         return string("['percent_parametrs', {'max_perсent':'") + std::to_string(max_perсent) + "'}]";
+     }
+
+     string delay_parametrs(uint32_t delay_clear_old_ref) {
+         return string("['delay_parametrs', {'delay_clear_old_ref':'") + std::to_string(delay_clear_old_ref) + "'}]";
+     }
 };
 
 
--- tests/golos.referral_tests.cpp
+++ tests/golos.referral_tests.cpp
@@ -1,7 +1,6 @@
 #include "golos_tester.hpp"
 #include "golos.referral_test_api.hpp"
 #include "contracts.hpp"
-#include "../golos.referral/include/golos.referral/config.hpp"
 
 
 namespace cfg = golos::config;
@@ -9,25 +8,28 @@ using namespace eosio::testing;
 using namespace eosio::chain;
 using namespace fc;
 
-
-class golos_referral_tester : public golos_tester {
-protected:
-    golos_referral_api referral;
-
+class golos_referral_tester : public extended_tester {
 public:
-
     golos_referral_tester()
-        : golos_tester(cfg::referral_name)
-        , referral({this, cfg::referral_name})
+        : extended_tester(cfg::referral_name)
+        , referral( {this, cfg::referral_name} )
     {
         create_accounts({N(sania), N(pasha), N(tania), N(vania), N(issuer), _code});
-        produce_blocks(2);
+        step(2);
 
         install_contract(_code, contracts::referral_wasm(), contracts::referral_abi());
     }
 
-protected:
+    void init_params() {
+        auto breakout_parametrs = referral.breakout_parametrs(min_breakout, max_breakout);
+        auto expire_parametrs   = referral.expire_parametrs(max_expire);
+        auto percent_parametrs  = referral.percent_parametrs(max_persent);
 
+        auto params = "[" + breakout_parametrs + "," + expire_parametrs + "," + percent_parametrs + "]";
+        BOOST_CHECK_EQUAL(success(), referral.set_params(cfg::referral_name, params));
+    }
+
+protected:
     // TODO: make contract error messages more clear
     struct errors: contract_error_messages {
         const string referral_exist = amsg("A referral with the same name already exists");
@@ -35,30 +37,73 @@ protected:
         const string min_expire     = amsg("expire < current block time");
         const string max_expire     = amsg("expire > current block time + max_expire");
         const string min_breakout   = amsg("breakout <= min_breakout");
-        const string max_breakout   = amsg("breakout > min_breakout");
+        const string max_breakout   = amsg("breakout > max_breakout");
         const string persent        = amsg("specified parameter is greater than limit");
+
+        const string min_more_than_max = amsg("min_breakout > max_breakout");
+        const string negative_minimum  = amsg("min_breakout < 0");
+        const string negative_maximum  = amsg("max_breakout < 0");
+        const string limit_persent     = amsg("max_perсent > 100.00%");
     } err;
+
+    const asset min_breakout = asset(10000,  symbol(4,"GLS"));
+    const asset max_breakout = asset(100000, symbol(4,"GLS"));
+    const uint64_t max_expire = 600; // 600 sec
+    const uint32_t max_persent = 5000; // 50.00%
+
+    golos_referral_api referral;
 };
 
 BOOST_AUTO_TEST_SUITE(golos_referral_tests)
 
+ BOOST_FIXTURE_TEST_CASE(set_params, golos_referral_tester) try {
+     BOOST_TEST_MESSAGE("Test vesting parameters");
+     BOOST_TEST_MESSAGE("--- prepare");
+     step();
+
+     BOOST_TEST_MESSAGE("--- check that global params not exist");
+     BOOST_TEST_CHECK(referral.get_params().is_null());
+
+     init_params();
+     step();
+
+     auto obj_params = referral.get_params();
+     BOOST_TEST_MESSAGE("--- " + fc::json::to_string(obj_params));
+     BOOST_TEST_CHECK(obj_params["breakout_params"]["min_breakout"].as<asset>() == min_breakout);
+     BOOST_TEST_CHECK(obj_params["breakout_params"]["max_breakout"].as<asset>() == max_breakout);
+     BOOST_TEST_CHECK(obj_params["expire_params"]["max_expire"].as<uint64_t>() == max_expire);
+     BOOST_TEST_CHECK(obj_params["percent_params"]["max_perсent"].as<uint32_t>() == max_persent);
+
+     auto params = "[" + referral.breakout_parametrs(max_breakout, min_breakout) + "]";
+     BOOST_CHECK_EQUAL(err.min_more_than_max, referral.set_params(cfg::referral_name, params));
+
+     params = "[" + referral.breakout_parametrs(asset(-1, symbol(4,"GLS")), max_breakout) + "]";
+     BOOST_CHECK_EQUAL(err.negative_minimum, referral.set_params(cfg::referral_name, params));
+
+     params = "[" + referral.percent_parametrs(10001) + "]";
+     BOOST_CHECK_EQUAL(err.limit_persent, referral.set_params(cfg::referral_name, params));
+
+ } FC_LOG_AND_RETHROW()
+
  BOOST_FIXTURE_TEST_CASE(create_referral_tests, golos_referral_tester) try {
      BOOST_TEST_MESSAGE("Test creating referral");
-     auto time_now = static_cast<uint32_t>(time(nullptr));
 
+     init_params();
+     auto time_now = static_cast<uint32_t>(time(nullptr));
      BOOST_CHECK_EQUAL(err.referral_equal, referral.create_referral(N(issuer), N(issuer), time_now, 0, asset(10000, symbol(4, "GLS"))));
 
      BOOST_CHECK_EQUAL(err.min_expire, referral.create_referral(N(issuer), N(sania), time_now, 0, asset(10000, symbol(4, "GLS"))));
      BOOST_CHECK_EQUAL(err.max_expire, referral.create_referral(N(issuer), N(sania), time_now + 5 /*5 sec*/, 999999999999, asset(10000, symbol(4, "GLS"))));
 
-     BOOST_CHECK_EQUAL(err.min_breakout, referral.create_referral(N(issuer), N(sania), 500, time_now + 60 /*60 sec*/, asset(0, symbol(4, "GLS"))));
-     BOOST_CHECK_EQUAL(err.max_breakout, referral.create_referral(N(issuer), N(sania), 500, time_now + 60 /*60 sec*/, asset(110000, symbol(4, "GLS"))));
+     auto expire = 8; // sec
+     BOOST_CHECK_EQUAL(err.min_breakout, referral.create_referral(N(issuer), N(sania), 500, cur_time().to_seconds() + expire, asset(0, symbol(4, "GLS"))));
+     BOOST_CHECK_EQUAL(err.max_breakout, referral.create_referral(N(issuer), N(sania), 500, cur_time().to_seconds() + expire, asset(110000, symbol(4, "GLS"))));
 
-     BOOST_CHECK_EQUAL(err.persent, referral.create_referral(N(issuer), N(sania), 9500, time_now + 60 /*60 sec*/, asset(110000, symbol(4, "GLS"))));
+     BOOST_CHECK_EQUAL(err.persent, referral.create_referral(N(issuer), N(sania), 9500, cur_time().to_seconds() + expire, asset(50000, symbol(4, "GLS"))));
 
-     BOOST_CHECK_EQUAL(success(), referral.create_referral(N(issuer), N(sania), 500, time_now + 60 /*60 sec*/, asset(50000, symbol(4, "GLS"))));
+     BOOST_CHECK_EQUAL(success(), referral.create_referral(N(issuer), N(sania), 500, cur_time().to_seconds() + expire, asset(50000, symbol(4, "GLS"))));
 
-     BOOST_CHECK_EQUAL(err.referral_exist, referral.create_referral(N(issuer), N(sania), 500, time_now + 60 /*60 sec*/, asset(50000, symbol(4, "GLS"))));
+     BOOST_CHECK_EQUAL(err.referral_exist, referral.create_referral(N(issuer), N(sania), 500, cur_time().to_seconds() + expire, asset(50000, symbol(4, "GLS"))));
 
  } FC_LOG_AND_RETHROW()
 
