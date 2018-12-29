838c331c2f953ed769b4ef42d7d1b7ff05e85742
Semen Medvedev <smedvedev@golos.io>
2018-12-29 23:06:49 +0700

    Update CI scripts

--- .buildkite/steps/deploy-test.sh
+++ .buildkite/steps/deploy-test.sh
@@ -2,6 +2,22 @@
 
 set -euo pipefail
 
-# Run unit-tests
+docker stop mongo || true
+docker rm mongo || true
+docker volume rm cyberway-mongodb-data || true
+docker volume create --name=cyberway-mongodb-data
 
-exit 0
+cd Docker
+
+IMAGETAG=${BUILDKITE_BRANCH:-master}
+
+docker-compose up -d
+
+# Run unit-tests
+sleep 10s
+docker run --network docker_golos-contracts-net -ti cyberway/golos.contracts:$IMAGETAG  /bin/bash -c 'MONGO_URL=mongodb://mongo:27017 /opt/golos.contracts/unit_test'
+result=$?
+
+docker-compose down
+
+exit $result
