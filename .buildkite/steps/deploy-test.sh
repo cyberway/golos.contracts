63c015b1a2c8742d405ea43ec2d9a5e13671aa2e
Semen Medvedev <smedvedev@golos.io>
2019-01-09 15:11:58 +0700

    Fix network name

--- .buildkite/steps/deploy-test.sh
+++ .buildkite/steps/deploy-test.sh
@@ -15,7 +15,7 @@ docker-compose up -d
 
 # Run unit-tests
 sleep 10s
-docker run --network docker_golos-contracts-net -ti cyberway/golos.contracts:$IMAGETAG  /bin/bash -c 'MONGO_URL=mongodb://mongo:27017 /opt/golos.contracts/unit_test'
+docker run --network golos-tests_contracts-net -ti cyberway/golos.contracts:$IMAGETAG  /bin/bash -c 'MONGO_URL=mongodb://mongo:27017 /opt/golos.contracts/unit_test'
 result=$?
 
 docker-compose down
