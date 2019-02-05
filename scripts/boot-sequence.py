362debb0b27faa82b3abaee3b17636e123d81969
Semen Medvedev <smedvedev@golos.io>
2019-02-05 17:01:09 +0700

    Automate testnet deployment #431

--- scripts/boot-sequence.py
+++ scripts/boot-sequence.py
@@ -3,18 +3,22 @@ import os
 import sys
 import subprocess
 
+default_contracts_dir = '/opt/cyberway/bin/data-dir/contracts/'
+
 args = {
     'basedir': os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-    'cleos':'/opt/cyberway/bin/cleos --url=http://nodeosd:8888 ',
+    'cleos':'/opt/cyberway/bin/cleos --url=%s ' % os.environ.get('CYBERWAY_URL', 'http://nodeosd:8888'),
     'public_key':'GLS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV',
     'private_key':'5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3',
     'eosio_private_key':'5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3',
     'contracts_dir':'/opt/cyberway/bin/data-dir/contracts/',
+    'cyberway_contracts_dir': os.environ.get('CYBERWAY_CONTRACTS', default_contracts_dir),
+    'golos_contracts_dir': os.environ.get('GOLOS_CONTRACTS', default_contracts_dir),
 }
 
 
 bios_boot_sequence=('{basedir}/cyberway.contracts/scripts/bios-boot-sequence/bios-boot-sequence.py '
-                     '--cleos "{cleos}" --contracts-dir "{contracts_dir}" '
+                     '--cleos "{cleos}" --contracts-dir "{cyberway_contracts_dir}" '
                      '--public-key {public_key} --private-key {private_key} '
                      '--docker --all').format(**args)
 if subprocess.call(bios_boot_sequence, shell=True):
@@ -22,7 +26,7 @@ if subprocess.call(bios_boot_sequence, shell=True):
     sys.exit(1)
 
 golos_boot_sequence=('{basedir}/scripts/golos-boot-sequence/golos-boot-sequence.py '
-                     '--cleos "{cleos}" --contracts-dir "{contracts_dir}" ' 
+                     '--cleos "{cleos}" --contracts-dir "{golos_contracts_dir}" ' 
                      '--public-key {public_key} --private-key {private_key} '
                      '--eosio-private-key {eosio_private_key} '
                      '--docker --all').format(**args) 
