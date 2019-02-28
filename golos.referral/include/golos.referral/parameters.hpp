7bf0cceabb892370bfa79a3aaf4ce262612e5975
Alexander Dedkov <sanya.dedckov@yandex.ru>
2019-02-28 15:42:12 +0300

    add set_asset_param

--- golos.referral/include/golos.referral/parameters.hpp
+++ golos.referral/include/golos.referral/parameters.hpp
@@ -10,14 +10,22 @@ namespace golos {
 using namespace eosio;
 
 struct breakout_parametrs : parameter {
-   asset min_breakout = asset(0, symbol("GLS",3));
-   asset max_breakout = asset(0, symbol("GLS",3));
+   asset min_breakout;
+   asset max_breakout;
 
    void validate() const override {
         eosio_assert(min_breakout <= max_breakout, "min_breakout > max_breakout");
         eosio_assert(min_breakout.amount >= 0, "min_breakout < 0");
         eosio_assert(max_breakout.amount >= 0, "max_breakout < 0");
    }
+
+   template<typename T>
+   bool compare(const T& other) const {
+       return (this->max_breakout.symbol != other.max_breakout.symbol) &&
+              (this->min_breakout.symbol != other.min_breakout.symbol) &&
+              (this->min_breakout.amount != other.min_breakout.amount) &&
+              (this->min_breakout.amount != other.min_breakout.amount);
+   };
 };
 using breakout_param = param_wrapper<breakout_parametrs,2>;
 
