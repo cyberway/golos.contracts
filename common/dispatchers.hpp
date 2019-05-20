7b6d17b189276a5f7ee13e9e250a597c7630381a
Alexander Dedkov <sanya.dedckov@yandex.ru>
2019-05-20 13:02:29 +0300

    not testing

--- common/dispatchers.hpp
+++ common/dispatchers.hpp
@@ -1,8 +1,32 @@
 #pragma once
 
 #include "config.hpp"
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
 #include <eosiolib/dispatcher.hpp>
 
+template<typename T, typename... Args>
+bool bulk_execute_action( eosio::name self, eosio::name code, void (T::*func)(Args...) ) {
+    struct transfer_st {
+        eosio::name from;
+        std::vector<eosio::token::recipient> recipients;
+    };
+    auto data = eosio::unpack_action_data<transfer_st>();
+
+    for (auto recipient : data.recipients) {
+        std::tuple<std::decay_t<Args>...> args{data.from, recipient.to, recipient.quantity, recipient.memo};
+        eosio::datastream<const char*> ds = eosio::datastream<const char*>(nullptr, 0);
+        ds << args;
+
+        T inst(self, code, ds);
+        auto f2 = [&]( auto... a ){
+            ((&inst)->*func)( a... );
+        };
+
+        boost::mp11::tuple_apply( f2, args );
+    }
+    return true;
+}
 
 #define DISPATCH_WITH_TRANSFER(TYPE, TRANSFER, MEMBERS) \
 extern "C" { \
@@ -11,9 +35,10 @@ extern "C" { \
             switch (action) { \
                 EOSIO_DISPATCH_HELPER(TYPE, MEMBERS) \
             } \
-            /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
         } else if (code == golos::config::token_name.value && action == "transfer"_n.value) { \
             eosio::execute_action(eosio::name(receiver), eosio::name(code), &TYPE::TRANSFER); \
+        } else if (code == golos::config::token_name.value && action == "bulktransfer"_n.value) { \
+            bulk_execute_action(eosio::name(receiver), eosio::name(code), &TYPE::TRANSFER); \
         } \
    } \
 } \
