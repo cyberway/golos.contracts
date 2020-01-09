948c9fe33da95342d985bf5638670099caf34cae
Evgeny Schepachev <schepachev@gmail.com>
2020-01-09 12:07:34 +0700

    Golos contracts sources copied into the image instead of cloning from git #936

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 
 IMAGETAG=${BUILDKITE_BRANCH:-master}
 BRANCHNAME=${BUILDKITE_BRANCH:-master}
+REVISION=$(git rev-parse HEAD)
+
+pushd cyberway.contracts
+
+CYBERWAY_CONTRACTS_REVISION=$(git rev-parse HEAD)
+
+popd
 
 TAGREF=${BUILDKITE_TAG:+"tags/$BUILDKITE_TAG"}
 REF=${TAGREF:-"heads/$BUILDKITE_BRANCH"}
@@ -15,5 +22,4 @@ else
     BUILDTYPE="latest"
 fi
 
-cd Docker
-docker build -t cyberway/golos.contracts:${IMAGETAG} --build-arg branch=${BRANCHNAME} --build-arg buildtype=${BUILDTYPE} --build-arg ref=${REF} .
+docker build -t cyberway/golos.contracts:${IMAGETAG} --build-arg buildtype=${BUILDTYPE} --build-arg=verison=${REVISION} --build-arg=cyberway_contracts_version=${CYBERWAY_CONTRACTS_REVISION}  -f Docker/Dockerfile .
