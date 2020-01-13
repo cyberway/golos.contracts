d7963c206b9fe24fbdf43281b1e7b49326b0c455
Evgeny Schepachev <schepachev@gmail.com>
2020-01-13 19:29:22 +0700

    cyberway.contracts are taken from a docker image when building a golos.contracts image #938

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,12 +3,6 @@ set -euo pipefail
 
 REVISION=$(git rev-parse HEAD)
 
-pushd cyberway.contracts
-
-CYBERWAY_CONTRACTS_REVISION=$(git rev-parse HEAD)
-
-popd
-
 if [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
     BUILDTYPE="stable"
 else
@@ -18,4 +12,6 @@ fi
 CDT_TAG=${CDT_TAG:-$BUILDTYPE}
 CW_TAG=${CW_TAG:-$BUILDTYPE}
 BUILDER_TAG=${BUILDER_TAG:-$BUILDTYPE}
-docker build -t cyberway/golos.contracts:${REVISION} --build-arg=cw_tag=${CW_TAG} --build-arg=cdt_tag=${CDT_TAG} --build-arg=cyberway_contracts_version=${CYBERWAY_CONTRACTS_REVISION}  --build-arg=builder_tag=${BUILDER_TAG} --build-arg=version=${REVISION} -f Docker/Dockerfile .
+SYSTEM_CONTRACTS_TAG=${SYSTEM_CONTRACTS_TAG:-$BUILDTYPE}
+
+docker build -t cyberway/golos.contracts:${REVISION} --build-arg=cw_tag=${CW_TAG} --build-arg=cdt_tag=${CDT_TAG} --build-arg=system_contracts_tag=${SYSTEM_CONTRACTS_TAG} --build-arg=builder_tag=${BUILDER_TAG} --build-arg=version=${REVISION} -f Docker/Dockerfile .
