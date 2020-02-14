e2f1d85b03bab8da035a373b19f0b44902d65676
boostmultifruit <boostmultifruit@mail.ru>
2020-02-14 14:40:36 +0000

    Remove manual ABI files #943

---
+++ common/config.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+// TODO: Fully migrate to _n and remove
+#ifndef UNIT_TEST_ENV
+// test env still use N macro, so we continue to use it here
+#   define STRINGIFY_(x) #x
+#   define STRINGIFY(x) STRINGIFY_(x)
+#   define N(x) eosio::name(STRINGIFY(x))
+#endif
+
+#ifdef UNIT_TEST_ENV
+#   include <eosio/chain/types.hpp>
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu-string-literal-operator-template"
+template <typename T, T... Str>
+inline eosio::chain::name operator ""_n() {
+   return eosio::chain::name({Str...});
+}
+#pragma clang diagnostic pop
+#else
+#include <cyber.token/cyber.token.hpp>
+using token = eosio::token;
+#endif
+
+#define CYBER_TOKEN "cyber.token"
+#define GOLOS_VESTING "gls.vesting"
+
+namespace golos { namespace config {
+
+// contracts
+static const auto control_name  = "gls.ctrl"_n;
+static const auto social_name   = "gls.social"_n;
+static const auto publish_name  = "gls.publish"_n;
+static const auto charge_name   = "gls.charge"_n;
+static const auto emission_name = "gls.emit"_n;
+static const auto workers_name  = "gls.worker"_n;
+static const auto referral_name = "gls.referral"_n;
+
+static const auto internal_name = "cyber"_n;
+static const auto token_name    = "cyber.token"_n;
+static const auto vesting_name  = "gls.vesting"_n;
+
+// permissions
+static const auto code_name = "code"_n;
+static const auto owner_name = "owner"_n;
+static const auto active_name = "active"_n;
+static const auto invoice_name = "invoice"_n;          // for golos.vesting:retire
+static const auto issue_name = "issue"_n;              // for golos.emit:emit
+static const auto issuer_name = "issuer"_n;
+static const auto changevest_name = "changevest"_n;    // for golos.ctrl:changevest
+static const auto one_name = "witn.one"_n;
+static const auto super_majority_name = "witn.smajor"_n;
+static const auto majority_name = "witn.major"_n;
+static const auto minority_name = "witn.minor"_n;
+
+// numbers and time
+static constexpr auto _1percent = 100;
+static constexpr auto _100percent = 100 * _1percent;
+static constexpr auto block_interval_ms = 3000;//1000 / 2;
+static constexpr int64_t blocks_per_year = int64_t(365)*24*60*60*1000/block_interval_ms;
+
+} // config
+
+constexpr int64_t time_to_blocks(int64_t time) {
+    return time / (1000 * config::block_interval_ms);
+}
+constexpr int64_t seconds_to_blocks(int64_t sec) {
+    return time_to_blocks(sec * 1e6);
+}
+
+} // golos::config
