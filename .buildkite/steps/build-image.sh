1197e423b65b03c1ed8d5f61b749c8256397c43d
e-schepachev <44019320+e-schepachev@users.noreply.github.com>
2020-03-02 16:30:49 +0700

    Update base projects from a docker hub before usage in a docker conta… (#955)
    * Update base projects from a docker hub before usage in a docker container #954
    
    * Adeded +x to the if conditions #954

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -9,9 +9,24 @@ else
     BUILDTYPE="latest"
 fi
 
-CDT_TAG=${CDT_TAG:-$BUILDTYPE}
-CW_TAG=${CW_TAG:-$BUILDTYPE}
-BUILDER_TAG=${BUILDER_TAG:-$BUILDTYPE}
-SYSTEM_CONTRACTS_TAG=${SYSTEM_CONTRACTS_TAG:-$BUILDTYPE}
+if [[ -z ${CDT_TAG+x} ]]; then 
+    CDT_TAG=${BUILDTYPE}
+    docker pull cyberway/cyberway.cdt:${CDT_TAG}
+fi
+
+if [[ -z ${CW_TAG+x} ]]; then 
+    CW_TAG=${BUILDTYPE}
+    docker pull cyberway/cyberway:${CW_TAG}
+fi
+
+if [[ -z ${BUILDER_TAG+x} ]]; then 
+    BUILDER_TAG=${BUILDTYPE}
+    docker pull cyberway/builder:${BUILDER_TAG}
+fi
+
+if [[ -z ${SYSTEM_CONTRACTS_TAG+x} ]]; then 
+    SYSTEM_CONTRACTS_TAG=${BUILDTYPE}
+    docker pull cyberway/cyberway.contracts:${SYSTEM_CONTRACTS_TAG}
+fi
 
 docker build -t cyberway/golos.contracts:${REVISION} --build-arg=cw_tag=${CW_TAG} --build-arg=cdt_tag=${CDT_TAG} --build-arg=system_contracts_tag=${SYSTEM_CONTRACTS_TAG} --build-arg=builder_tag=${BUILDER_TAG} --build-arg=version=${REVISION} -f Docker/Dockerfile .
